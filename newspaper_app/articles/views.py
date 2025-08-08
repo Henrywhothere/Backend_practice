54cec36b69bb29d0f646eea80aad672e0455a751
HenrY <113247707+Henrywhothere@users.noreply.github.com>
2025-08-09 05:35:06 +0700

    newspaper app practice

---
+++ newspaper_app/articles/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Article
+
+# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ('title','body',)
+
+class ArticleDeleteView(DeleteView):
+    model = Article 
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'author', 'body',)
